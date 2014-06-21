a568ec56282c54d2b6d788248048776c2f7ab297
Asumu Takikawa <asumu@ccs.neu.edu>
2014-06-21 12:51:10 -0400

    Initial commit

---
+++ inspect-syntax/main.rkt
@@ -0,0 +1,33 @@
+#lang racket/base
+
+(require racket/format
+         racket/pretty)
+
+(provide inspect-syntax)
+
+;; This module provides utility functions for examining syntax objects
+
+(define rule (make-string 50 #\;))
+(define (prule) (displayln rule))
+
+(define (inspect-syntax stx)
+  (define datum (syntax->datum stx))
+  (define keys (syntax-property-symbol-keys stx))
+
+  (prule)
+  (displayln (~a ";; Inspecting syntax object: " (~a stx #:max-width 20)))
+  (newline)
+  (displayln "Pretty-printed syntax:")
+  (pretty-print datum)
+  (newline)
+  (displayln (~a "Source: "   (~a #:width 20 #:align 'right (syntax-source stx))))
+  (displayln (~a "Line: "     (~a #:width 22 #:align 'right (syntax-line stx))))
+  (displayln (~a "Column: "   (~a #:width 20 #:align 'right (syntax-column stx))))
+  (displayln (~a "Position: " (~a #:width 18 #:align 'right (syntax-position stx))))
+  (displayln (~a "Span: "     (~a #:width 22 #:align 'right (syntax-span stx))))
+  (newline)
+  (displayln "Properties:")
+  (for ([key (in-list keys)])
+    (displayln (~a "  " key ": " (syntax-property key))))
+  (newline)
+  (prule))
